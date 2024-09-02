cc9e6a3874293a8825babdf2f4fa92c02238f9a8
codal-umistri <umistri@codal.com>
2024-09-02 15:38:43 +0530

    chore: added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        suse: ['SUSE', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
